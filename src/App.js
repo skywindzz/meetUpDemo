054da4d2bcbc840d9ea009c3b05a0b5d845e31b5
Anthony Liang <anthliang@fb.com>
2022-05-30 15:49:04 -0700

    5/30 section 471

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import { Route, Switch } from 'react-router-dom';
-import AllMeetupsPage from "./pages/AllMeetups";
-import Favorites from "./pages/Favorites";
-import NewMeetup from "./pages/NewMeetup";
+import AllMeetupsPage from './pages/AllMeetups';
+import Favorites from './pages/Favorites';
+import NewMeetup from './pages/NewMeetup';
 import MainNavigation from './componenets/layout/MainNavigation';
 
 function App() {
   //Switch tells the differnt route only one of them will be displayed at the same time, if you don't do that the base path component and the other component path you call up will be display at the same time.
-  
+
   //if you don't use exact on the home path, once switch function find the home path it will stop because it's the first on the list. If you want to stop that, you need to use exact on the home route so the function will keep on looking until it founds the exact name path.
-    return (
-      <div>
+  return (
+    <div>
       <MainNavigation />
-      <Switch> 
-        <Route path='/' exact={ true }>
-        <AllMeetupsPage />
-      </Route>
-        
-      <Route path='/favorites'>
-        <Favorites />
-      </Route>
-    
-      <Route path='/new-meetup'>
-        <NewMeetup />
-      </Route>
+      <Switch>
+        <Route path='/' exact={true}>
+          <AllMeetupsPage />
+        </Route>
+
+        <Route path='/favorites'>
+          <Favorites />
+        </Route>
+
+        <Route path='/new-meetup'>
+          <NewMeetup />
+        </Route>
       </Switch>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/componenets/layout/MainNavigation.js
+++ src/componenets/layout/MainNavigation.js
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 //why you need Link, if you use the usually javascript a href, it sends a request to the server and get the page yiour reuested, however since you've already load the whole app with react, you can use this Link tag to tell react to show the component when needed.
 
+import classes from './MainNavigation.module.css'
+
 function MainNavigation() {
   return (
-    <header>
-      <div>React meetup</div>
+    <header className={classes.header}>
+      <div className={ classes.logo}>React meetup</div>
       <nav>
         <ul>
           <li>
